c3eecccd819a945b763d2d4dd7a2c5406a01c536
Ivette <ivparejo@gmail.com>
2021-02-23 15:34:30 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
-export default () => {
+import React from "react"
+import data from "./data/data.json"
+import Linia from "./components/Linia"
+
+function App() {
+    const LiniaComponents = data.map(data =>
+    <Linia key={data.id} text={data.text} />) 
 
     return (
         <div>
-            hello World!
+            {LiniaComponents}
         </div>
-    );
-};
+    )
+}
+
+export default App
---
+++ src/components/Linia.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+function Linia(props){
+    console.log(props)
+
+    return(
+        <h4>{props.text}</h4>
+    )
+}
+export default Linia
