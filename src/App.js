8cee1eee6dcf5d2dffa835f4bfa6ed33c5bbc28d
Ivette <ivparejo@gmail.com>
2021-02-24 16:43:17 +0100

    Local server added

--- src/App.js
+++ src/App.js
@@ -1,18 +1,67 @@
-import React from "react"
-import data from "./data/data.json"
-import Linia from "./components/Linia"
+import React, {Component} from "react"
+import data from "./db/db.json"
+// import Linia from "./components/Linia"
 import StyledComponents from "./components/StyledComponents"
 
-function App() {
-    const LiniaComponents = data.map(data =>
-    <Linia key={data.id} text={data.text} />) 
-
-    return (
-        <div>   
-            <StyledComponents/>
-            {LiniaComponents}      
-        </div>
-    )
+class App extends Component {
+    constructor() {
+        super()
+        this.state = {
+            loading: false,
+            textTheatre: []
+        }
+        // this.handleChange = this.handleChange.bind(this)
+    }
+    
+    componentDidMount() {
+        this.setState({loading: true})
+        fetch("http://localhost:3000/")
+            .then(response => response.json())
+            .then(data => { console.log(data)
+                this.setState({
+                    loading: false,
+                    textTheatre: data
+                })
+            })
+    }
+    
+    render() {
+        const text = this.state.loading ? "Loading..." : this.state.textTheatre.text
+        return (
+            <div>
+                <StyledComponents/>
+                {/* <button>Previous</button>
+                <button>After</button> */}
+                <p>{text}</p>
+            </div>
+        )
+    }
 }
 
 export default App
+
+
+// function App (){
+//     // const LiniaComponents = data.map(data =>
+//     //     <Linia key={data.id} text={data.text} />) 
+    
+//     const [data,setData]=useState([]);
+    
+//     const getData=()=>{
+//         fetch('/data/data.json')
+//             .then(response => response.json())
+//             .then(data => console.log(data))
+//     }
+
+//     useEffect(()=>{ getData()},[])
+    
+//     return (
+//         <div> 
+//             <StyledComponents/>
+//             <button>Previous</button>
+//             <button>After</button>
+//             {/* {data && data.length>0 && data.map((item)=><p>{item.about}</p>)} */}
+//             {/* {LiniaComponents}       */}
+//         </div>
+//     )
+// }
--- src/components/Linia.js
+++ src/components/Linia.js
@@ -1,5 +1,5 @@
 import React from "react"
-import StyledComponents from "./StyledComponents"
+// import React from "react"
 
 function Linia(props){
     return(
