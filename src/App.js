3c6eccd9d9088b21b81d5a3f33d1402aea9c2288
Ivette <ivparejo@gmail.com>
2021-03-09 16:22:28 +0100

    onClick changes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,51 @@
-import React from "react"
-import TextListData from "./components/TextList";
+import React, {useState, useEffect} from "react"
+import axios from "axios";
 
 function App () {
+    const [list, setList] = useState([]); 
+    const [index, setIndex] = useState(0);
+    const [currentText, setCurrentText] = useState("");
 
-    // function increment() {
-    //     setCount(prevCount => prevCount + 1)
-    // }
+    //Fetching local data (API)
+    useEffect(() => {
+        axios
+            .get('http://localhost:3000/dataText')
+            .then(response => {
+                console.log(response.data)
+                setList(response.data)
+
+            })
+            .catch((error) => {
+                console.log(error);
+                alert("New Error")
+            });
+    }, []);
+
+    // HANDLE PREVIOUS 
+    const handlePrev = ()=> {
+        if(index <= 0) {    
+
+        }else {
+            setIndex(index - 1);
+            setCurrentText(list[index].text)
+        }
+    };
+
+    // HANDLE NEXT 
+    const handleNext= () => {
+        if(index >= list.length){ 
+           
+        }else {
+            setIndex(index + 1);
+            setCurrentText(list[index].text)
+        }
+    };  
+
     
-    // function decrement() {
-    //     setCount(prevCount => prevCount - 1)
-    // }    
+
+    //const listItems = list.map((e) =>
+    //    <Linia key={e.id} text={e.text} />
+    //);
 
     return (
         <div className="App">
@@ -17,15 +53,17 @@ function App () {
                 <h1 className="App-title">The Theatre App</h1>
                 <h4 className="App-subtitle">Click on the buttons below to see different theatrical texts.</h4>
                 <div className="buttons">
-                    <button className="btn">Previous</button>
-                    <button className="btn">After</button>
-                    {/* <button onClick={decrement} className="btn">Previous</button> */}
-                    {/* <button onClick={increment} className="btn">After</button> */}
+                    <button className="btn" onClick={() => handlePrev()}>Previous Line</button>
+                    <button className="btn" onClick={() => handleNext()}>Next Line  </button>
                 </div>
+               
             </header>  
+
             <div className="App-section1">
-                <TextListData />
+                {/* Index: {index} <br/> */}
+                {currentText}
             </div> 
+            
             <div className="App-section2">
                 <img className="App-img" src="/img/forms.png"  alt="logo" />  
             </div>        
@@ -34,4 +72,5 @@ function App () {
 }
 
 export default App
+
 
